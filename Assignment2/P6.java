7271d6b51d2db1f0dcfafca988642d2450e3dca6
Shreyas5050 <113713387+Shreyas5050@users.noreply.github.com>
2022-09-27 00:55:31 +0530

    Add files via upload

---
+++ Assignment2/P6.java
@@ -0,0 +1,17 @@
+class P3{
+	public static void main(String args[]){
+		int c=0;
+		for (int i=1;i<=5;i++)
+		{
+			 (int j=1;j<=i;j++)
+			{
+				//c=c+j;
+				System.out.print("* ");
+				
+			}
+			
+			c=0;
+			System.out.println();
+		}
+	}
+}
